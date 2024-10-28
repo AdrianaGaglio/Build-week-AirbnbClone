a72ad48255c2fcd1176a3124f739e656b66a5e36
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2024-10-28 17:03:14 +0100

    fix components

--- airbnb-clone/src/app/pages/host/host.module.ts
+++ airbnb-clone/src/app/pages/host/host.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HostRoutingModule } from './host-routing.module';
 import { HostComponent } from './host.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ReviewComponent } from './components/review/review.component';
 
 @NgModule({
-  declarations: [HostComponent],
+  declarations: [HostComponent, ReviewComponent],
   imports: [CommonModule, HostRoutingModule, SharedModule],
 })
 export class HostModule {}
