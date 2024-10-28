fe0d16da1e90d3e4bcb33b29eae9aa6c6d886642
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-28 16:49:45 +0100

    Add modal popup

--- airbnb-clone/src/app/auth/auth.module.ts
+++ airbnb-clone/src/app/auth/auth.module.ts
@@ -6,9 +6,15 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedmodalModule } from '../shared/sharedmodal/sharedmodal.module';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    SharedmodalModule,
+  ],
 })
 export class AuthModule {}
