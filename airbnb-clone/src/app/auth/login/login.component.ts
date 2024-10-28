10a7a0f70b6c77f9a38061357c60e8cb4ee3d04c
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-28 13:19:57 +0100

    wip - login form

--- airbnb-clone/src/app/auth/login/login.component.ts
+++ airbnb-clone/src/app/auth/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private fb: FormBuilder) {}
 
+  loginForm!: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email]),
+      password: this.fb.control('', [Validators.required]),
+    });
+  }
+
+  isInvalidTouched(fieldName: string) {
+    return (
+      this.loginForm.get(fieldName)?.invalid &&
+      this.loginForm.get(fieldName)?.touched
+    );
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
