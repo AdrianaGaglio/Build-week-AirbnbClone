f6f977c122917bb75bd835ea59155d7224141fea
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-28 10:59:55 +0100

    first commit

---
+++ airbnb-clone/src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'apartment', loadChildren: () => import('./pages/apartment/apartment.module').then(m => m.ApartmentModule) }, { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }, { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
