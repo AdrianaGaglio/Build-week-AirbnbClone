9376c832e57e5ebe4071460830193194d3b9f471
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-29 11:43:08 +0100

    wip - favourites

--- airbnb-clone/src/app/shared/card/card.component.ts
+++ airbnb-clone/src/app/shared/card/card.component.ts
@@ -1,14 +1,52 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { iApartment } from '../../interfaces/iapartment';
+import { FavouritesService } from '../../services/favourites.service';
+import { iUser } from '../../interfaces/iuser';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  constructor(
+    private favSvc: FavouritesService,
+    private authSvc: AuthService
+  ) {}
+
   @Input() apartment!: iApartment;
+  isAppreciated!: boolean;
   isFavorite = false;
+  rating!: number;
+  user!: iUser;
+
+  ngOnInit() {
+    this.authSvc.isLoggedIn$.subscribe((isLoggedIn) => {
+      if (isLoggedIn) {
+        this.user = this.authSvc.authResponse$.getValue()!.user!;
+        this.favSvc
+          .checkIfPresent(this.user.id, this.apartment)
+          .subscribe((isPresent) => {
+            this.isFavorite = isPresent;
+          });
+      }
+    });
+
+    this.rating = Math.floor(
+      this.apartment.ratings.vote / this.apartment.ratings.count
+    );
+    if (this.rating > 3) {
+      this.isAppreciated = true;
+    }
+  }
+
+  addToFavourite() {
+    this.favSvc.addToFavourite(this.user.id, this.apartment).subscribe();
+  }
+
+  removeFromFavourite() {}
 
   toggleFavorite() {
     this.isFavorite = !this.isFavorite;
