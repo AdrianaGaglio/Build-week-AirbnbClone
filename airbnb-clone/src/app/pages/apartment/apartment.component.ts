653b20b60761b1b3d5c823d48c1a65cb423a19ad
Stevenozzo <stevencarlino46@live.com>
2024-10-29 11:35:32 +0100

    added apartment page

--- airbnb-clone/src/app/pages/apartment/apartment.component.ts
+++ airbnb-clone/src/app/pages/apartment/apartment.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-apartment',
   templateUrl: './apartment.component.html',
-  styleUrl: './apartment.component.scss'
+  styleUrls: ['./apartment.component.scss'],
 })
 export class ApartmentComponent {
+  isFavorite: boolean = false;
 
+  toggleFavorite(): void {
+    this.isFavorite = !this.isFavorite;
+  }
 }
--- airbnb-clone/src/app/pages/apartment/apartment.module.ts
+++ airbnb-clone/src/app/pages/apartment/apartment.module.ts
@@ -4,16 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ApartmentRoutingModule } from './apartment-routing.module';
 import { ApartmentComponent } from './apartment.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
-
+import { NgIconsModule } from '@ng-icons/core';
+import { ionHeart, ionHeartOutline } from '@ng-icons/ionicons';
 
 @NgModule({
-  declarations: [
-    ApartmentComponent,
-    ReservationComponent
-  ],
+  declarations: [ApartmentComponent, ReservationComponent],
   imports: [
     CommonModule,
-    ApartmentRoutingModule
-  ]
+    ApartmentRoutingModule,
+    NgIconsModule.withIcons({ ionHeart, ionHeartOutline }),
+  ],
 })
-export class ApartmentModule { }
+export class ApartmentModule {}
