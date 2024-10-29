7db6a4d76abd73f94ecbde2d2488750ea23f63d6
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-29 12:54:23 +0100

    move apartments on mockapi

--- airbnb-clone/src/environments/environment.ts
+++ airbnb-clone/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   registerUrl: 'http://localhost:3000/register',
   loginUrl: 'http://localhost:3000/login',
   userUrl: 'http://localhost:3000/users',
-  apartmentsUrl: 'http://localhost:3000/apartments',
+  apartmentsUrl: 'https://671f5051e7a5792f052dcafa.mockapi.io/apartments',
   favouritesUrl: 'http://localhost:3000/favourites',
   logo: 'https://images-ext-1.discordapp.net/external/jD0Gjp_45OhK5u15FOSEH_GYXcbKycyJf-q1SqHzCQs/https/upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%25C3%25A9lo.svg/1200px-Airbnb_Logo_B%25C3%25A9lo.svg.png?format=webp&quality=lossless&width=1114&height=348',
   firebaseConfig: {
