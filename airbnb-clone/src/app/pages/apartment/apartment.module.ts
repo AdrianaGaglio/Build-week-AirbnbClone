7774a53de9451b92cc2738b31c355b69708b53d0
Stevenozzo <stevencarlino46@live.com>
2024-10-29 16:58:53 +0100

    fixied aprtment page

--- airbnb-clone/src/app/pages/apartment/apartment.module.ts
+++ airbnb-clone/src/app/pages/apartment/apartment.module.ts
@@ -5,14 +5,30 @@ import { ApartmentRoutingModule } from './apartment-routing.module';
 import { ApartmentComponent } from './apartment.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
 import { NgIconsModule } from '@ng-icons/core';
-import { ionHeart, ionHeartOutline } from '@ng-icons/ionicons';
+import {
+  ionHeart,
+  ionHeartOutline,
+  ionBedOutline,
+  ionWifiOutline,
+  ionTvOutline,
+  ionSnowOutline,
+  ionCafeOutline,
+} from '@ng-icons/ionicons';
 
 @NgModule({
   declarations: [ApartmentComponent, ReservationComponent],
   imports: [
     CommonModule,
     ApartmentRoutingModule,
-    NgIconsModule.withIcons({ ionHeart, ionHeartOutline }),
+    NgIconsModule.withIcons({
+      ionHeart,
+      ionHeartOutline,
+      ionBedOutline,
+      ionWifiOutline,
+      ionTvOutline,
+      ionSnowOutline,
+      ionCafeOutline,
+    }),
   ],
 })
 export class ApartmentModule {}
