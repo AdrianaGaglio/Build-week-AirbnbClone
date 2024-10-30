62edc7fcd60d4010745360890f7ace6fa9999daf
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-30 08:23:26 +0100

    wip - message function

--- airbnb-clone/src/app/shared/reservation/reservation.component.ts
+++ airbnb-clone/src/app/shared/reservation/reservation.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { iApartment } from '../../interfaces/iapartment';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from '../../services/message.service';
 
 @Component({
   selector: 'app-reservation',
@@ -9,29 +11,38 @@ import { AuthService } from '../../auth/auth.service';
   styleUrl: './reservation.component.scss',
 })
 export class ReservationComponent {
-  constructor(private fb: FormBuilder, private authSvc: AuthService) {}
+  constructor(
+    private fb: FormBuilder,
+    private authSvc: AuthService,
+    private route: ActivatedRoute,
+    private messageSvc: MessageService
+  ) {}
 
   @Input() apartment!: iApartment;
 
   isApartment!: boolean;
   commentForm!: FormGroup;
   senderId!: string;
+  receiverId!: string;
+  message!: string;
 
   ngOnInit() {
     if (this.apartment) {
       this.isApartment = true;
+      this.receiverId = this.apartment.hostId;
+    } else {
+      this.route.params.subscribe((params) => {
+        this.receiverId = params['id'];
+      });
     }
-
     this.authSvc.authState$.subscribe((user) => {
-      if (user) {
+      if (user && this.receiverId) {
         this.senderId = user.uid;
         this.commentForm = this.fb.group({
           senderId: this.fb.control(this.senderId),
-          receiverId: this.fb.control(
-            this.apartment.hostId ? this.apartment.hostId : null
-          ),
+          receiverId: this.fb.control(this.receiverId),
           message: this.fb.control('', Validators.required),
-          apartment: this.fb.control(this.apartment ? this.apartment : null),
+          apartment: this.fb.control(this.apartment ? this.apartment : ''),
           isRead: this.fb.control(false),
         });
       }
@@ -39,6 +50,15 @@ export class ReservationComponent {
   }
 
   sendMessage() {
-    console.log(this.commentForm.value);
+    if (this.commentForm.valid) {
+      this.messageSvc.sendMessage(this.commentForm.value).subscribe({
+        next: (message) => {
+          this.message = 'Messaggio inviato correttamente';
+        },
+        error: (err) => {
+          this.message = err;
+        },
+      });
+    }
   }
 }
