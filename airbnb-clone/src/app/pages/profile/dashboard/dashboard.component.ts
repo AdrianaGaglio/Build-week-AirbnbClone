6d676b9abbd9ef652ffe78d7bcb92dea5824a507
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-31 12:34:52 +0100

    When delete apartment, remove alse the file on the storage

--- airbnb-clone/src/app/pages/profile/dashboard/dashboard.component.ts
+++ airbnb-clone/src/app/pages/profile/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../../services/user.service';
 import { ApartmentService } from '../../../services/apartment.service';
 import { iApartment } from '../../../interfaces/iapartment';
 import { AuthService } from '../../../auth/auth.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,7 +15,8 @@ export class DashboardComponent {
   constructor(
     private router: Router,
     private apartmentSvc: ApartmentService,
-    private authSvc: AuthService
+    private authSvc: AuthService,
+    private storage: AngularFireStorage
   ) {}
 
   apartments!: iApartment[];
@@ -39,11 +41,37 @@ export class DashboardComponent {
 
   deleteApartment(id: number) {
     this.apartmentSvc.deleteApartmentById(id).subscribe();
+    const found = this.apartments.find((data) => data.id === id);
+    if (found) {
+      found.coverImage.forEach((imageUrl) => {
+        this.deliteImgFromStorage(imageUrl);
+      });
+    }
     this.apartments = this.apartments.filter((data) => data.id !== id);
   }
 
   checkOut(apartment: iApartment) {
     apartment.availability = true;
     this.apartmentSvc.changeAvailability(apartment).subscribe();
+  }
+
+  deliteImgFromStorage(imageUrl: string) {
+    const baseUrl =
+      'https://firebasestorage.googleapis.com/v0/b/buildweek-82174.appspot.com/o/';
+    const filePath = decodeURIComponent(
+      imageUrl.split(baseUrl)[1].split('?')[0]
+    );
+
+    this.storage
+      .ref(filePath)
+      .delete()
+      .subscribe({
+        next: () => {
+          console.log('Immagine eliminata con successo');
+        },
+        error: (error) => {
+          console.error("Errore durante l'eliminazione dell'immagine:", error);
+        },
+      });
   }
 }
