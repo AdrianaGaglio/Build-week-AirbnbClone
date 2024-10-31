78d910e0c1c7d10f93f3b74e17c278f25d3e0920
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-10-31 10:50:16 +0100

    fixed messages page

--- airbnb-clone/src/app/pages/profile/messages/messages.component.ts
+++ airbnb-clone/src/app/pages/profile/messages/messages.component.ts
@@ -16,6 +16,7 @@ export class MessagesComponent {
   ngOnInit() {
     this.messageSvc.allMessages$.subscribe((msgs) => {
       if (msgs.length > 0) {
+        console.log(msgs);
         this.messages = msgs;
         this.messages = msgs.sort((a: iMessage, b: iMessage) => {
           // Ordina prima per 'isRead' (dove i non letti - false - vengono per primi)
